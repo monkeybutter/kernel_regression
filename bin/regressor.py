464513a4692b9bae0a08d4fe154a1a2e79bba0c7
Pablo Rozas-Larraondo <p.rozas.larraondo@gmail.com>
2014-05-08 08:55:15 +1000

    Double regression added

--- bin/regressor.py
+++ bin/regressor.py
@@ -17,10 +17,14 @@ def test(file_path):
     df = df[np.isfinite(df['WindSpd'])]
 
     print('___________')
-    print(me_no_regression(df, 'MetarwindSpeed', 'WindSpd'))
-    print(rmse_no_regression(df, 'MetarwindSpeed', 'WindSpd'))
-    simple_regression(df, 'MetarwindSpeed', 'WindSpd')
+    #print('ME no regression: {}'.format(me_no_regression(df, 'MetarwindSpeed', 'WindSpd')))
+    #print('RMSE no regression: {}'.format(rmse_no_regression(df, 'MetarwindSpeed', 'WindSpd')))
+    #print('ME simple regression: {}'.format(me_simple_regression(df, 'MetarwindSpeed', 'WindSpd')))
+    #print('RMSE simple regression: {}'.format(rmse_simple_regression(df, 'MetarwindSpeed', 'WindSpd')))
 
+    params = double_linear_regression(df, 'MetarwindSpeed', 'WindSpd', 'SWRad')
+    print(params)
+    print('{}, {}'.format(params[0, 0], params[0, 1], params[0, 2]))
 
 def me_no_regression(df, col1, col2):
     """
@@ -39,9 +43,9 @@ def rmse_no_regression(df, col1, col2):
 
     :param file_path:
     """
-    values = (df[col1] - df[col2])**2
+    se = ((df[col1] - df[col2])**2).sum()
 
-    return math.sqrt(values.sum()/df.shape[0])
+    return math.sqrt(se/df.shape[0])
 
 
 def plot_simple_regression(df, col1, col2):
@@ -75,32 +79,84 @@ def plot_simple_regression(df, col1, col2):
     plt.show()
 
 
-def simple_regression(df, col1, col2):
+def simple_linear_regression(df, y_name, x_name):
 
-    df = df[[col2, col1]]
+    df = df[[x_name, y_name]]
     data = np.matrix(df)
 
-    y = np.matrix(np.array(df[col1]))
-    x0 = np.array(df[col2])
+    y = np.matrix(np.array(df[y_name]))
+    x0 = np.array(df[x_name])
     x = np.matrix(np.vstack((x0, np.ones(x0.shape[0]))))
 
     b = y * x.T * np.linalg.inv(x*x.T)
     return b
 
-def me_simple_regression(df, col1, col2):
+def me_simple_regression(df, y_name, x_name):
 
-    params = simple_regression(df, col1, col2)
-    me = 0
+    params = simple_linear_regression(df, y_name, x_name)
 
-    for :
-        me += math.fabs(yList.get(i)-(x0List.get(i)*params[0]+params[1]))
+    e = 0
 
-    return me/yList.size()
+    for index, row in df.iterrows():
+        #print(index)
+        e += math.fabs(row[y_name]-(row[x_name]*params[0, 0]+params[0, 1]))
+
+    return e/df.shape[0]
+
+
+def rmse_simple_regression(df, y_name, x_name):
+
+    params = simple_linear_regression(df, y_name, x_name)
+
+    se = 0
+
+    for index, row in df.iterrows():
+        #print(index)
+        se += math.fabs(row[y_name]-(row[x_name]*params[0, 0]+params[0, 1]))**2
+
+    return math.sqrt(se/df.shape[0])
+
+
+def weighted_linear_regression(df, y_name, x_name, x_centre, width):
+
+    df = df[[x_name, y_name]]
+    data = np.matrix(df)
+
+    y = np.matrix(np.array(df[y_name]))
+    x0 = np.array(df[x_name])
+    x = np.matrix(np.vstack((x0, np.ones(x0.shape[0]))))
+
+    w = np.matrix(np.zeros(shape=(x0.shape[0], x0.shape[0])))
+
+    for i in range(x0.shape[0]):
+        for j in range(x0.shape[0]):
+            if i == j:
+                if math.fabs(x0[i]-x_centre) < width:
+                    w[i, j] = 1.0
+                else:
+                    w[i, j] = 0.0
+
+    b = y * w * x.T * np.linalg.inv(x * w * x.T)
+    return b
+
+
+def double_linear_regression(df, y_name, x0_name, x1_name):
+
+    df = df[[x0_name, x1_name, y_name]]
+    data = np.matrix(df)
+
+    y = np.matrix(np.array(df[y_name]))
+    x0 = np.array(df[x0_name])
+    x1 = np.array(df[x1_name])
+    x = np.matrix(np.vstack((x0, x1, np.ones(x0.shape[0]))))
+
+    b = y * x.T * np.linalg.inv(x*x.T)
+    return b
+
 
 def degrees_distance(angleA, angleB):
 
     return min(math.fabs(angleA-angleB), 360-math.fabs(angleA-angleB))
-
 
 
 if __name__ == "__main__":
